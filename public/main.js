4fd2821436329dc0822cf8639d4810491213abaa
19Andrei97 <dredre1997@gmail.com>
2021-05-16 19:26:12 +0300

    Added Music

--- public/main.js
+++ public/main.js
@@ -6,55 +6,42 @@ let user, table;
 let socket = io();
 
 let code;
-socket.on('gameCode', handleGameCode);
-
-
+socket.on("gameCode", handleGameCode);
 
 socket.on("changeEnemies", (name, cards) => {
   if (name !== Person) {
-    table.updateMove(name, cards)
+    table.updateMove(name, cards);
   }
 });
 // Server Place Card from 14 deck
 socket.on("placeFrom14", (card, where) => {
-  
   // Show card on Table & prepend class
   let where1 = $(document).find(`#${where}`);
   let classes = where1.attr("class");
 
-  where1
-    .removeClass()
-    .addClass(card[1])
-    .addClass(classes)
-    .text(card[0])
+  where1.removeClass().addClass(card[1]).addClass(classes).text(card[0]);
 
   if (card[0] === 10) this.lockTen(where1);
 });
 
 // Server Place Card from normal deck
 socket.on("placeFromRest", (card, where) => {
-
   // Show card on Table & prepend class
   let where1 = $(document).find(`#${where}`);
   let classes = where1.attr("class");
 
-  where1
-    .removeClass()
-    .addClass(card[1])
-    .addClass(classes)
-    .text(card[0])
+  where1.removeClass().addClass(card[1]).addClass(classes).text(card[0]);
 
   if (card[0] === 10) this.lockTen(where1);
 });
 
 function handleGameCode(gameCode) {
-  $('#gameCodeDisplay').html(gameCode);
+  $("#gameCodeDisplay").html(gameCode);
   code = gameCode;
 }
 
 socket.on("startGame", (data, Players) => {
   PlayerListVar = Players;
-
 
   var newHTML = $.map(PlayerListVar, function (value) {
     return `<dl style='margin-bottom:0px !important;'><i style='margin-right:5px;' class='fas fa-user-astronaut' style='font-size:36px'></i>${value}</dl>`;
@@ -68,53 +55,74 @@ $("#joinGameButton").click(function () {
 
   code = gameCodeInput.value;
   Person = prompt("Please enter your name", "");
-  if(Person === '') Person = 'Guest'
+  if (Person === "") Person = "Guest";
   socket.emit("joinGame", code, Person);
   $("#new-game-div").fadeOut();
   setTimeout(() => {
     $("#PlayersDiv").fadeIn();
   }, 500);
+  $("audio").each(function () {
+    this.play(); // Start playing
+    this.currentTime = 0; // Reset time
+  });
 });
 
 // Start Button
 $("#new-game-btn").click(function () {
   Person = prompt("Please enter your name", "");
-  if(Person === '') {
-    Person = 'Guest'
+  if (Person === "") {
+    Person = "Guest";
   }
   socket.emit("startGameOne", Person);
 
-  $("#Playerlist").html(`<dl style='margin-bottom:0px !important;'><i style='margin-right:5px;' class='fas fa-user-astronaut' style='font-size:36px'></i>${Person}</dl>`);
+  $("#Playerlist").html(
+    `<dl style='margin-bottom:0px !important;'><i style='margin-right:5px;' class='fas fa-user-astronaut' style='font-size:36px'></i>${Person}</dl>`
+  );
   PlayerNumber = 1;
   $("#new-game-div").fadeOut();
 
   setTimeout(() => {
     $("#PlayersDiv").fadeIn();
   }, 500);
+
+  $("audio").each(function () {
+    this.play(); // Start playing
+    this.currentTime = 0; // Reset time
+  });
 });
 
 $("#start-btn").click(function () {
-  socket.emit('startGameTogether', code);
+  socket.emit("startGameTogether", code);
+  $("audio").each(function () {
+    $(this).animate({ volume: 0 }, 2000, function () {
+      this.pause(); // Stop playing
+      this.currentTime = 0; // Reset time
+    });
+  });
 });
 
-
-socket.on('startGameNow', () => {
-  
-  if (PlayerListVar){
+socket.on("startGameNow", () => {
+  if (PlayerListVar) {
     OtherPlayers = PlayerListVar.slice();
-    OtherPlayers.splice( $.inArray(Person,OtherPlayers) ,1 );
-    OtherPlayers[0] ? $('#nameOne').html(OtherPlayers[0]) : $('#nameOne').hide();
-    OtherPlayers[1] ? $('#nameTwo').html(OtherPlayers[1]) : $('#nameTwo').hide();
-    OtherPlayers[2] ? $('#nameThree').html(OtherPlayers[2]) : $('#nameThree').hide();
+    OtherPlayers.splice($.inArray(Person, OtherPlayers), 1);
+    OtherPlayers[0]
+      ? $("#nameOne").html(OtherPlayers[0])
+      : $("#nameOne").hide();
+    OtherPlayers[1]
+      ? $("#nameTwo").html(OtherPlayers[1])
+      : $("#nameTwo").hide();
+    OtherPlayers[2]
+      ? $("#nameThree").html(OtherPlayers[2])
+      : $("#nameThree").hide();
   }
 
   $("#start-div").fadeOut();
-  user = new User(new Deck(),Person, code);
+  user = new User(new Deck(), Person, code);
 
   // If there are other Players pass the number and the array
-  if(PlayerListVar){
+  if (PlayerListVar) {
     table = new Table(PlayerListVar.length, OtherPlayers);
-    table.showOpponents($('#enemies-div'))
+    table.showOpponents($("#enemies-div"));
   } else {
     table = new Table(1);
   }
@@ -133,7 +141,7 @@ $("#deck").click(function () {
   user.fromDeck($("#deck-col-add"));
 });
 
-// Put Card when slot on table is selected 
+// Put Card when slot on table is selected
 $(".center-table").on("click", ".slot-table", function () {
   $(".slot-table").removeClass("selected");
 
